a9f8ebbf9700aa53546ae1f279cf48c13d773fc7
trudova <liubovtrudova@gmail.com>
2020-08-14 14:26:42 -0400

    more inputs

--- src/Form.js
+++ src/Form.js
@@ -4,7 +4,10 @@ export default class Form extends Component {
 state={
     firstName:'',
     lastName:'',
-    people:[]
+    age:'',
+    gender:'',
+    weight:'',
+    info:[]
 }
 handleChange = (event)=>{
     const value =event.target.value
@@ -16,12 +19,18 @@ handleSubmit=(e)=>{
     e.preventDefault();
     const firstName=this.state.firstName;
     const lastName= this.state.lastName;
-    if(firstName.length>0 && lastName.length>0){
-        const person=`${firstName} ${lastName} .`;
+    const age= this.state.age;
+    const gender= this.state.gender;
+    const weight= this.state.lastName;
+    if(firstName.length>0 && lastName.length>0 && age.length>0 && gender.length>0 && weight.length>0){
+        const dog=`${firstName} ${lastName} ${age} ${gender} ${weight}.`;
         this.setState({
-            people:[...this.state.people, person],
+            info:[...this.state.info, dog],
             firstName:'',
-            lastName:''
+            lastName:'',
+            age:'',
+            gender:'',
+            weight:''
         })
     }
 }
@@ -31,15 +40,35 @@ handleSubmit=(e)=>{
           <section>
               <article>
 <form onSubmit={this.handleSubmit}>
+   <label for='firstName'>Neme of the dog:</label>
     <input type='text'
      name='firstName'
       value={this.state.firstName} 
       onChange={this.handleChange}/>
-
+<label for='lastName'>Type of the dog:</label>
     <input type='text'
      name='lastName'
      value={this.state.lastName} 
       onChange={this.handleChange}/>
+
+<label for='age'>Neme of the dog:</label>
+    <input type='text'
+     name='age'
+      value={this.state.age} 
+      onChange={this.handleChange}/>
+
+      <label for='gender'>Neme of the dog:</label>
+    <input type='text'
+     name='gender'
+      value={this.state.gender} 
+      onChange={this.handleChange}/>
+
+<label for='weight'>Neme of the dog:</label>
+    <input type='text'
+     name='weight'
+      value={this.state.weight} 
+      onChange={this.handleChange}/>
+
     <button type='submit'>submit</button>
 </form>
               </article>
@@ -47,7 +76,7 @@ handleSubmit=(e)=>{
               <article>
                   <h1> Information about your dog</h1>
                   <div>
-                {this.state.people}
+                {this.state.info}
                   </div>
               </article>
           </section>   
